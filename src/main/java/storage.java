76f7bff47d4c135ca143fadaab61e926d1192630
Danielyan86 <516495459@qq.com>
2021-04-04 21:46:47 +0800

    add files

---
+++ src/main/java/storage.java
@@ -0,0 +1,11 @@
+public class storage {
+    static String storageMethod(String s) {
+        return s;
+    }
+
+    public static void main(String[] args) {
+        String a = "this is a string";
+        String b = storageMethod(a);
+        System.out.println(b);
+    }
+}
