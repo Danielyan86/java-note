14d0e12ea11e96422c5026d40cb3a8e6ff26f5be
Danielyan86 <516495459@qq.com>
2021-04-05 11:25:53 +0800

    add files

---
+++ src/main/java/Equivalence.java
@@ -0,0 +1,8 @@
+public class Equivalence {
+    public static void main(String[] args) {
+        Integer n1 = 47;
+        int n2 = 47;
+        System.out.println(n1.equals(n2));
+        System.out.println(n1 != n2);
+    }
+}
