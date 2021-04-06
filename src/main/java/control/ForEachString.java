904b5a89d4d1562967be227e31a3d33373dd543d
Danielyan86 <516495459@qq.com>
2021-04-06 22:19:50 +0800

    add files

---
+++ src/main/java/control/ForEachString.java
@@ -0,0 +1,8 @@
+package control;
+
+public class ForEachString {
+    public static void main(String[] args) {
+        for(char c :"an africa swallow".toCharArray())
+            System.out.println(c+"");
+    }
+}
