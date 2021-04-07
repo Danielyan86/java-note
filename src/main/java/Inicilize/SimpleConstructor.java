6103f13031417506b571718df8ee7f677e6c84b1
Danielyan86 <516495459@qq.com>
2021-04-07 23:23:39 +0800

    add files

---
+++ src/main/java/Inicilize/SimpleConstructor.java
@@ -0,0 +1,15 @@
+package Inicilize;
+
+class Rock {
+    Rock() {
+        System.out.println("this is the simple constructor");//it is like python __init__ constructor method
+    }
+}
+
+public class SimpleConstructor {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++)
+            new Rock();
+    }
+}
