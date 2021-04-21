d80cda40f445a3af47e31fb69dbf8a10d94bf959
Danielyan86 <516495459@qq.com>
2021-04-21 22:12:15 +0800

    add files

--- src/main/java/FinalKeyWord.java
+++ src/main/java/FinalKeyWord.java
@@ -1,5 +1,5 @@
 public class FinalKeyWord {
-    public final void test(){
-        System.out.println("can't be override with final keyword")
+    public final void test() {
+        System.out.println("can't be override with final keyword");
     }
 }
