1db85f092ecd19f787fd8f06df5d6847096b0e7e
Danielyan86 <516495459@qq.com>
2021-05-02 23:22:19 +0800

    update

---
+++ src/main/java/basic/commonbasicclass/StringBasic.java
@@ -0,0 +1,16 @@
+package basic.commonbasicclass;
+
+public class StringBasic {
+    public static void main(String[] args) {
+
+        String name = "old horse say programming";
+        String name1 = new String("old horse say programming");
+        String name2 = "olde horse";
+        System.out.println(name2);
+        name2 = name2 + "say programming";
+        System.out.println(name2);
+        System.out.println(name);
+        System.out.println(name1);
+    }
+
+}
