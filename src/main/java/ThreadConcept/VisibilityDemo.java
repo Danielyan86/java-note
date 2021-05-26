c29b475fbed90c7489ecc35efe6d805bfa64ee60
Danielyan86 <516495459@qq.com>
2021-05-26 21:47:00 +0800

    update

--- src/main/java/ThreadConcept/VisibilityDemo.java
+++ src/main/java/ThreadConcept/VisibilityDemo.java
@@ -2,20 +2,22 @@ package ThreadConcept;
 
 public class VisibilityDemo {
     private static boolean shutdown = false;
-    static class HelloThread extends Thread{
+
+    static class HelloThread extends Thread {
         @Override
         public void run() {
-            while (!shutdown){
+            while (!shutdown) {
                 System.out.println("do sth");
             }
             System.out.println("exit hello");
         }
     }
-    public static void main(String[] args) throws InterruptedException{
+
+    public static void main(String[] args) throws InterruptedException {
         new HelloThread().start();
         Thread.sleep(1000);
         shutdown = true;
         System.out.println("exit main");
     }
-//actually this program is exited successfully.
+    //actually this program is exited successfully.
 }
