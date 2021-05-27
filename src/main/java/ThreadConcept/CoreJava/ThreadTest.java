a478ee9e8aa5ab221d64b8deda5641767ff8d186
Danielyan86 <516495459@qq.com>
2021-05-27 21:55:47 +0800

    update

---
+++ src/main/java/ThreadConcept/CoreJava/ThreadTest.java
@@ -0,0 +1,23 @@
+package ThreadConcept.CoreJava;
+
+public class ThreadTest {
+    public static final int DELAY = 10;
+    public static final int STEPS = 100;
+    public static final double MAX_AMOUNT = 1000;
+
+    public static void main(String[] args) {
+        var bank = new Bank(4, 100000);
+        Runnable task1 = () -> {
+            try {
+                for (int i = 0; i < STEPS; i++) {
+                    double amount = MAX_AMOUNT * Math.random();
+                    bank.transfer(0, 1, amount);
+                    Thread.sleep((int) (DELAY * Math.random()));
+                }
+            } catch (InterruptedException e) {
+                System.out.println("this is exception part");
+            }
+        };
+        new Thread(task1).start();
+    }
+}
