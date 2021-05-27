52ca1caea850850aea7a0b4ff20f65e5d3a0ca24
Danielyan86 <516495459@qq.com>
2021-05-27 22:13:57 +0800

    update

--- src/main/java/ThreadConcept/CoreJava/Bank.java
+++ src/main/java/ThreadConcept/CoreJava/Bank.java
@@ -5,20 +5,25 @@ import java.util.Arrays;
 public class Bank {
     private final double[] accounts;
 
+    //初始化一个数组，每个元素为一个账户余额
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
         Arrays.fill(accounts, initialBalance);
+        System.out.println(Arrays.toString(accounts));
     }
 
+    //从一个账户向另外一个账户转账
     public void transfer(int from, int to, double amount) {
         if (accounts[from] < amount) return;
         System.out.print(Thread.currentThread());
         accounts[from] -= amount;
-        System.out.printf("%10.2f from %d to %d", amount, from, to);
+        System.out.printf("%10.2f from %d to %d ", amount, from, to);
         accounts[to] += amount;
+        System.out.println(Arrays.toString(accounts));
         System.out.printf("Total balance: %10.2f%n", getTotalBalance());
     }
 
+    //获取所有账户总金额，总金额不会变
     public double getTotalBalance() {
         double sum = 0;
         for (double a : accounts)
--- src/main/java/ThreadConcept/CoreJava/ThreadTest.java
+++ src/main/java/ThreadConcept/CoreJava/ThreadTest.java
@@ -3,10 +3,10 @@ package ThreadConcept.CoreJava;
 public class ThreadTest {
     public static final int DELAY = 10;
     public static final int STEPS = 100;
-    public static final double MAX_AMOUNT = 1000;
+    public static final double MAX_AMOUNT = 100;
 
     public static void main(String[] args) {
-        var bank = new Bank(4, 100000);
+        var bank = new Bank(4, 10000);
         Runnable task1 = () -> {
             try {
                 for (int i = 0; i < STEPS; i++) {
@@ -18,6 +18,18 @@ public class ThreadTest {
                 System.out.println("this is exception part");
             }
         };
+        Runnable task2 = () -> {
+            try {
+                for (int i = 0; i < STEPS; i++) {
+                    double amount = MAX_AMOUNT * Math.random();
+                    bank.transfer(0, 1, amount);
+                    Thread.sleep((int) (DELAY * Math.random()));
+                }
+            } catch (InterruptedException e) {
+                System.out.println("this is exception part");
+            }
+        };
         new Thread(task1).start();
+        new Thread(task2).start();
     }
 }
